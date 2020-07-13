98b78239ca7ed523501c6d46c939c63f8ce31cd7
AJ Speller <ajspeller@gmail.com>
2020-07-13 17:57:01 -0400

    Add server setup

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const morgan = require('morgan');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(morgan('dev'));
+
 
 app.listen(PORT, () => {
   console.log('Server listening on port: ', PORT);
