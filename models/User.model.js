9050ada9c2334246ef3acc6c57b442372bb1b21c
AJ Speller <ajspeller@gmail.com>
2020-07-13 18:46:26 -0400

    Add model validation

--- models/User.model.js
+++ models/User.model.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const minlength = [
+  3,
+  'The value of path `{PATH} (`{VALUE}`) must be ({minlength}) or more characters',
+];
+
 const UserSchema = new Schema({
   username: {
     type: String,
-    // required: true,
+    minlength: minlength,
+    required: true,
   },
   email: {
     type: String,
     unique: true,
-    // required: true,
+    required: true,
   },
   password: {
     type: String,
